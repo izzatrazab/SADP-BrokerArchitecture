938ec51d83c44cbbfc84151e6da5b05a1c950a02
Farid Adli <84564471+faridadli@users.noreply.github.com>
2022-06-21 15:04:27 +0800

    Add consumer and sender

---
+++ messaging-app/src/main/java/com/broker_architecture/Consumer.java
@@ -0,0 +1,25 @@
+package com.sadp;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Consumer {
+	public static void main(String[] args) throws IOException, TimeoutException{
+		ConnectionFactory factory = new ConnectionFactory();
+	  	
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		channel.queueDeclare("hello-world", false, false, false, null);
+		
+		
+		channel.basicConsume("hello-world", true, (consumerTag, message)->{
+			String m = new String(message.getBody(), "UTF-8");
+			System.out.println("Message received yehehe: " + m);
+		}, consumerTag -> {});
+		
+	}
+}
